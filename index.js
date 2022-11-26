58aa9f77080839cc0e7615f20c4c5f8e127b9645
sajthewizard <rsaj53@gmail.com>
2022-11-26 18:15:58 +0100

    mongodb connected

--- index.js
+++ index.js
@@ -11,7 +11,22 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pxpswgp.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run() {
+    try {
 
+    }
+    finally {
+        const productscollections = client.db('verkaufer').collection('products')
+        app.get('/products', async (req, res) => {
+            const query = {};
+            const options = await productscollections.find(query).toArray();
+            res.send(options);
+        })
+    }
+
+}
+
+run().catch(console.log);
 
 app.get('/', async (req, res) => {
     res.send('verkaufer is running')
