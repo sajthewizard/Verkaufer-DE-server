b3047860fa159314f7116e63a22069a4484822e5
sajthewizard <rsaj53@gmail.com>
2022-11-28 20:37:49 +0100

    seller created

--- index.js
+++ index.js
@@ -80,6 +80,18 @@ async function run() {
             const result = await bookigsCollections.insertOne(book);
             res.send(result);
         })
+        app.get('/users/seller/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email }
+            const user = await userscollections.findOne(query);
+            res.send({ isSeller: user?.role === 'seller' });
+        })
+        app.get('/users/admin/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email }
+            const user = await userscollections.findOne(query);
+            res.send({ isAdmin: user?.role === 'admin' });
+        })
         app.put('/users/admin/:id', verifyJWT, async (req, res) => {
             const decodedEmail = req.decoded.email;
             const query = { email: decodedEmail };
